b6db938385bb6ef8ca23c9133c5934e8cd4ce3b0
Azwar F <azwarfathullah@gmail.com>
2018-01-11 17:18:19 +0700

    Update Report.js

--- Nexigo/js/views/home/Report.js
+++ Nexigo/js/views/home/Report.js
@@ -47,7 +47,79 @@
         fields: [
             { type: 'button', text: 'Generate Report', cssClass: 'xg-btn', offset: 2, cols: 2, action: 'submit' },
         ]
+    }, {
+        type: 'row',
+        panels: [{
+            name: 'panel1',
+            offset: 1,
+            fields: [
+                {
+                    type: 'grid',
+                    text: 'Table',
+                    name: 'tasklist-grid',
+                    onDblClick: 'doubleClick',
+                    options: {
+                        sortable: false,
+                        editable: false,
+                        filterable: true,
+                        pageable: true,
+                        selectable: 'single',
+                    },
+                    fields: [
+                        {
+                            name: 'Name',
+                            text: 'Name',
+                            type: 'text'
+                        },
+                        {
+                            name: 'Email',
+                            text: 'Email',
+                            type: 'text'
+                        },
+                        {
+                            name: 'LeaveType',
+                            text: 'Leave Type',
+                            type: 'text'
+                        },
+                        {
+                            name: 'StartDate',
+                            text: 'Start Date',
+                            type: 'text'
+                        },
+                        {
+                            name: 'EndDate',
+                            text: 'End Date',
+                            type: 'text'
+                        },
+                        {
+                            name: 'DaysLeave',
+                            text: 'Jumlah Cuti',
+                            type: 'text'
+                        },
+                        {
+                            name: 'Submission',
+                            text: 'Submission',
+                            type: 'text'
+                        },
+                        {
+                            name: 'status',
+                            text: 'Status',
+                            type: 'text'
+                        },
+                    ],
+                    data: 'http://localhost:31602/api/FormLeave/ReportGrid',
+                }]
+        }]
     }],
+     
+    init: function (xg, callback) {
+        var filter = {
+            field: 'status',
+            operator: 'neq',
+            value: ''
+        };
+        xg.grid.instance('tasklist-grid').dataSource.filter(filter)
+    }, 
 
     Submit: function () {
         xg.loading.show();
